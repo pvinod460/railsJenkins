0283a38bb603611013e9b2d7652f4aa54e8a388a
sigmainfo <sigma@sigmainfo.net>
2014-04-15 06:37:43 -0400

    changed user

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :format, :pretty
 set :log_level, :debug
 set :pty, true
  
-set :user, 'deployer'
+set :user, 'vinodp'
  
 #set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}

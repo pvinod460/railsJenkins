954a35a16335e38fb221f85d747fe64e1307b08e
sigmainfo <sigma@sigmainfo.net>
2014-04-15 04:50:32 -0400

    modified configuraton files for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,56 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
+set :application, 'railsJenkins'
+set :repo_url, "https://github.com/pvinod460/railsJenkins.git"
+ 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# set :deploy_to, '/var/www/my_app'
+ 
+set :deploy_to, "/home/vinodp/railsJenkins"
 # set :scm, :git
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
-
+ 
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
+ 
+set :user, 'deployer'
+ 
+#set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+ 
+set :keep_releases, 5
+ 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  after :finishing, 'deploy:cleanup'
-
+ 
+desc 'Stop application'
+task :stop do
+on roles(:app), in: :sequence, wait: 5 do
+#execute "cd #{current_path} && bundle exec passenger stop -p 8080" rescue nil
+#execute "cd #{current_path} && bundle exec passenger stop" rescue nil
+end
+end
+ 
+desc 'Start application'
+task :start do
+on roles(:app), in: :sequence, wait: 5 do
+#execute "cd #{current_path} && bundle exec passenger start -d -p 8080 -e production --user=deploy"
+execute "cd #{current_path}"
+execute "bundle exec passenger start"
+end
+end
+ 
+desc 'Restart application'
+task :restart do
+invoke "deploy:stop"
+invoke "deploy:start"
+end
+ 
+after :restart, :clear_cache do
+on roles(:web), in: :groups, limit: 3, wait: 10 do
+# Here we can do anything such as:
+# within release_path do
+# execute :rake, 'cache:clear'
+# end
+end
+end
+ 
+after :finishing, 'deploy:cleanup'
+ 
 end
